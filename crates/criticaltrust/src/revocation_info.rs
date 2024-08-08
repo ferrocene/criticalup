6764eba5fca81651e156ff6db00948ba11ddd2b8
Amanjeev Sethi <amanjeev.sethi@ferrous-systems.com>
2024-08-08 10:24:02 -0400

    Review: naming improvement
    Co-authored-by: Ana Hobden <operator@hoverbear.org>

--- crates/criticaltrust/src/revocation_info.rs
+++ crates/criticaltrust/src/revocation_info.rs
@@ -40,7 +40,7 @@ mod tests {
     use time::macros::datetime;
 
     #[test]
-    fn debug_revocation_info() {
+    fn new_revocation_info() {
         let r = RevocationInfo::new(vec![vec![12, 21, 33]], datetime!(2025-08-05 00:00 UTC));
         assert_eq!("RevocationInfo { revoked_content_sha256: [[12, 21, 33]], expires_at: 2025-08-05 0:00:00.0 +00:00:00 }", format!("{:?}", r));
     }
