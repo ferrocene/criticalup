275ac9f92946544e49444e62e8e1636f820eeb45
Ana Hobden <ana.hobden@ferrous-systems.com>
2024-12-03 12:42:05 -0800

    Handle PATH/lib paths

--- crates/criticalup-cli/src/commands/which.rs
+++ crates/criticalup-cli/src/commands/which.rs
@@ -9,38 +9,25 @@ use std::path::PathBuf;
 
 pub(crate) async fn run(
     ctx: &Context,
-    binary: String,
+    tool: String,
     project: Option<PathBuf>,
 ) -> Result<(), Error> {
-    let found_binary = locate_binary(ctx, binary, project).await?;
-    println!("{}\n", found_binary.display());
-
-    Ok(())
-}
-
-#[tracing::instrument(level = "debug", skip_all, fields(binary, project))]
-pub(crate) async fn locate_binary(
-    ctx: &Context,
-    binary: String,
-    project: Option<PathBuf>,
-) -> Result<PathBuf, Error> {
     let manifest = ProjectManifest::get(project).await?;
 
     let installation_dir = &ctx.config.paths.installation_dir;
 
-    let mut found_binary = None;
     for product in manifest.products() {
         let abs_installation_dir_path = installation_dir.join(product.installation_id());
 
         let bin_path = PathBuf::from("bin");
 
         let mut tool_executable = PathBuf::new();
-        tool_executable.set_file_name(&binary);
+        tool_executable.set_file_name(&tool);
 
         let tools_bin_path = abs_installation_dir_path.join(bin_path.join(&tool_executable));
 
         if tools_bin_path.exists() {
-            found_binary = Some(tools_bin_path);
+            println!("{}\n", tools_bin_path.display());
         } else {
             // On Windows, the user can pass (for example) `cargo` or `cargo.exe`
             #[cfg(windows)]
@@ -48,15 +35,15 @@ pub(crate) async fn locate_binary(
                 let mut tools_bin_path_with_exe = tools_bin_path.clone();
                 tools_bin_path_with_exe.set_extension("exe");
                 if tools_bin_path_with_exe.exists() {
-                    found_binary = Some(tools_bin_path_with_exe);
+                    println!("{}\n", tools_bin_path_with_exe.display());
+                } else {
+                    return Err(BinaryNotInstalled(tool));
                 }
             }
+            #[cfg(not(windows))]
+            return Err(BinaryNotInstalled(tool));
         }
     }
 
-    if let Some(found_binary) = found_binary {
-        Ok(found_binary)
-    } else {
-        Err(BinaryNotInstalled(binary))
-    }
+    Ok(())
 }
