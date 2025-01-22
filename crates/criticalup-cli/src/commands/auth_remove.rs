286eb6ccce956a2e64f82d9b2c40da1839a6df62
Amanjeev Sethi <amanjeev.sethi@ferrous-systems.com>
2025-01-22 18:28:54 -0500

    Refactor to add error if env var is not utf-8 encoded.

--- crates/criticalup-cli/src/commands/auth_remove.rs
+++ crates/criticalup-cli/src/commands/auth_remove.rs
@@ -8,7 +8,7 @@ use criticalup_core::state::{EnvVars, State};
 pub(crate) async fn run(ctx: &Context) -> Result<(), Error> {
     let state = State::load(&ctx.config).await?;
 
-    let env_vars = EnvVars::default();
+    let env_vars = EnvVars::read().await?;
 
     if state.authentication_token(None, &env_vars).await.is_some() {
         state.set_authentication_token(None);
