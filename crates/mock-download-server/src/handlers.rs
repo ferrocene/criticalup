4742a5578e17b7b0530930ed56770a30387b118a
Ana Hobden <ana.hobden@ferrous-systems.com>
2025-05-20 12:58:53 -0700

    Almost working...

--- crates/mock-download-server/src/handlers.rs
+++ crates/mock-download-server/src/handlers.rs
@@ -3,8 +3,10 @@
 
 use crate::Serialize;
 use crate::{AuthenticationToken, Data};
+use md5::Digest;
 use criticaltrust::manifests::ManifestVersion;
 use tiny_http::{Header, Method, Request, Response, ResponseBox, StatusCode};
+use xz2::stream::Status;
 
 pub(crate) fn handle_request(data: &Data, req: &Request) -> ResponseBox {
     let url_parts = req
@@ -15,7 +17,7 @@ pub(crate) fn handle_request(data: &Data, req: &Request) -> ResponseBox {
 
     let resp = match (req.method(), url_parts.as_slice()) {
         (Method::Get, ["v1", "tokens", "current"]) => handle_v1_tokens_current(data, req),
-        (Method::Get, ["v1", "keys"]) => handle_v1_keys(data),
+        (Method::Get, ["v1", "keys"]) => handle_v1_keys(data, req),
         (Method::Get, ["v1", "releases", product, release]) => {
             handle_v1_release(data, product, release)
         }
@@ -57,12 +59,37 @@ fn handle_v1_tokens_current(data: &Data, req: &Request) -> Result<Resp, Resp> {
     Ok(Resp::json(token))
 }
 
-fn handle_v1_keys(data: &Data) -> Result<Resp, Resp> {
-    Ok(Resp::json(&criticaltrust::manifests::KeysManifest {
+fn handle_v1_keys(data: &Data, req: &Request) -> Result<Resp, Resp> {
+    let manifest = criticaltrust::manifests::KeysManifest {
         version: ManifestVersion,
         keys: data.keys.clone(),
         revoked_signatures: data.revoked_signatures.clone(),
-    }))
+    };
+
+    let if_none_match = req.headers().iter().find_map(|v| {
+        if v.field.as_str() == "if-none-match" {
+            Some(v.value.to_string())
+        } else {
+            None
+        }
+    });
+
+    if let Some(if_none_match) = if_none_match {
+        // If the user already has the item downloaded, they at one point were permitted to download it.
+        // We only validate that it is correct. This is not a license check.
+        let mut hasher = md5::Md5::new();
+        let json = serde_json::to_vec(&manifest).unwrap();
+        hasher.update(&json);
+        let etag_string = format!("{:x}", hasher.finalize());
+
+        let etag = format!(r#""{etag_string}""#);
+
+        panic!("{if_none_match} == {etag}");
+        if if_none_match == etag {
+            return Ok(Resp::NotModified);
+        }
+    }
+    Ok(Resp::json(&manifest))
 }
 
 fn handle_v1_release(data: &Data, product: &str, release: &str) -> Result<Resp, Resp> {
@@ -101,6 +128,7 @@ fn authorize<'a>(data: &'a Data, req: &Request) -> Result<&'a AuthenticationToke
 enum Resp {
     Forbidden,
     NotFound,
+    NotModified,
     Json(Vec<u8>),
     File(Vec<u8>),
 }
@@ -130,7 +158,7 @@ impl Resp {
                     Header::from_bytes(&b"Content-Disposition"[..], &b"attachment"[..]).unwrap(),
                 )
                 .boxed(),
-
+            Resp::NotModified => Response::empty(StatusCode(304)).boxed(),
             Resp::Forbidden => Response::empty(StatusCode(403)).boxed(),
             Resp::NotFound => Response::empty(StatusCode(404)).boxed(),
         }
